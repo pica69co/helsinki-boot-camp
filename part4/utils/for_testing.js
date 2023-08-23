f665a0c82cc16fdd1b80fe21ad25c5ef89e98787
pica69co <oscarwarrieta@gmail.com>
2023-08-23 12:54:31 -0500

    format

--- part4/utils/for_testing.js
+++ part4/utils/for_testing.js
@@ -1,17 +1,15 @@
 const palindrome = (string) => {
-    return string.split('').reverse().join('')
-}
+  return string.split("").reverse().join("");
+};
 
 const average = (Array) => {
-    const reducer = (sum, item) => {
-        return sum + item
-    }
-    return Array.length === 0
-        ? 0 
-        : Array.reduce(reducer, 0) / Array.length
-}
+  const reducer = (sum, item) => {
+    return sum + item;
+  };
+  return Array.length === 0 ? 0 : Array.reduce(reducer, 0) / Array.length;
+};
 
 module.exports = {
-    palindrome,
-    average,
-}
+  palindrome,
+  average,
+};
